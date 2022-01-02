7527527bd6736feba090cef42cc57ab6dc55694e
Maxwell Flitton <maxwellflitton@Maxwells-MacBook-Pro.local>
2022-01-02 16:36:17 +0000

    removing the target

--- food-app-engine/target/debug/deps/food_app_engine-c12ac00a9eafd9a5.d
+++
@@ -1,7 +0,0 @@
-/Users/maxwellflitton/Documents/github/rust-food-app/food-app-engine/target/debug/deps/food_app_engine-c12ac00a9eafd9a5: src/main.rs src/ingredients/mod.rs src/ingredients/ingredients_map.rs
-
-/Users/maxwellflitton/Documents/github/rust-food-app/food-app-engine/target/debug/deps/food_app_engine-c12ac00a9eafd9a5.d: src/main.rs src/ingredients/mod.rs src/ingredients/ingredients_map.rs
-
-src/main.rs:
-src/ingredients/mod.rs:
-src/ingredients/ingredients_map.rs:
--- food-app-engine/target/rls/debug/deps/food_app_engine-275a837ca446bbd4.d
+++
@@ -1,7 +0,0 @@
-/Users/maxwellflitton/Documents/github/rust-food-app/food-app-engine/target/rls/debug/deps/food_app_engine-275a837ca446bbd4.rmeta: src/main.rs src/ingredients/mod.rs src/ingredients/ingredients_map.rs
-
-/Users/maxwellflitton/Documents/github/rust-food-app/food-app-engine/target/rls/debug/deps/food_app_engine-275a837ca446bbd4.d: src/main.rs src/ingredients/mod.rs src/ingredients/ingredients_map.rs
-
-src/main.rs:
-src/ingredients/mod.rs:
-src/ingredients/ingredients_map.rs:
